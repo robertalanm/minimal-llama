a1447ff900c11b5fee157266cb2f844a37abafc1
Carro <robertmyers@hey.com>
2023-03-14 23:08:40 -0500

    add data

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
-deepspeed --num_gpus=8 finetune.py --config_path ./configs/base_configs/bpt.yaml \
+deepspeed --num_gpus=8 tune.py \
 --ds_config_path ./configs/ds_configs/ds_config_bpt.json \
 --deepspeed ./configs/ds_configs/ds_config_bpt.json
